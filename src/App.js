76da845bf48c814b2e355f71c4f41c88273aec84
Nicolas Oviedo <97.oviedo.nicolas@gmail.com>
2023-04-23 19:17:20 -0300

    proj final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./componets/Home/Home";
+import CartContent from "./componets/CartContent/CartContent";
+import DataProvider from "./componets/Context/DataContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA37WwlGYcr5_agtidgNk4hXPtwD9Z5ZfM",
+  authDomain: "proyectoreactnoviedo.firebaseapp.com",
+  projectId: "proyectoreactnoviedo",
+  storageBucket: "proyectoreactnoviedo.appspot.com",
+  messagingSenderId: "144020504060",
+  appId: "1:144020504060:web:dff34e782dc95d8c3e0679"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<CartContent />} />
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>Tienda de Flores</h1>
+        <p>Los mejores y mas hermosos ramos.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
